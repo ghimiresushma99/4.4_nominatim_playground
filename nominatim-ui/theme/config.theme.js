f3ee8cb6fd84ef89e43f13132d347bb03a5108be
Sushma <ghimiresushma99@gmail.com>
2024-04-29 11:31:11 +0545

    update nominatim 4.4

---
+++ nominatim-ui/theme/config.theme.js
@@ -0,0 +1,5 @@
+// Example file how to overwrite defaults from config.default.js
+// The file should be named 'config.theme.js'
+
+Nominatim_Config.Nominatim_API_Endpoint = 'http://localhost:8080/';
+Nominatim_Config.Page_Title = 'My Server demo';
